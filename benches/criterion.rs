15d84de7a99cf4db358009b61d723c742bc0017a
Sampo Siitonen <sampo.siitonen@gmail.com>
2022-09-03 18:43:40 +0300

    split up benchmarks

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -2,24 +2,22 @@ use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criteri
 
 use rand::{thread_rng, Rng};
 
-use im::Vector;
-
 use btreevec::BTreeVec;
 
-fn bench_get(c: &mut Criterion) {
+fn bench_get_bvec(c: &mut Criterion) {
     let mut rng = thread_rng();
 
-    for size in [1_000, 10_000, 100_000, 1_000_000] {
+    for size in [1_000, 10_000, 100_000, 1_000_000, 10_000_000] {
         let mut bvec = BTreeVec::<i32>::new();
-        let mut vec = Vec::new();
-        let mut im_vec = Vector::new();
+        // let mut vec = Vec::new();
+        // let mut im_vec = Vector::new();
 
         for x in 0..size as i32 {
             let i = rng.gen_range(0..=bvec.len());
 
             bvec.insert(i, x);
-            vec.insert(i, x);
-            im_vec.insert(i, x);
+            // vec.insert(i, x);
+            // im_vec.insert(i, x);
         }
 
         c.bench_with_input(
@@ -34,6 +32,42 @@ fn bench_get(c: &mut Criterion) {
             },
         );
 
+        // c.bench_with_input(
+        //     BenchmarkId::new("Vec<i32>::get (random)", size),
+        //     &size,
+        //     |b, &s| {
+        //         b.iter_batched(
+        //             || rng.gen_range(0..s),
+        //             |i| vec.get(i),
+        //             BatchSize::SmallInput,
+        //         )
+        //     },
+        // );
+
+        // c.bench_with_input(
+        //     BenchmarkId::new("im::Vector<i32>::get (random)", size),
+        //     &size,
+        //     |b, &s| {
+        //         b.iter_batched(
+        //             || rng.gen_range(0..s),
+        //             |i| im_vec.get(i),
+        //             BatchSize::SmallInput,
+        //         )
+        //     },
+        // );
+    }
+}
+
+fn bench_get_vec(c: &mut Criterion) {
+    let mut rng = thread_rng();
+
+    for size in [1_000, 10_000, 100_000, 1_000_000] {
+        let mut vec = Vec::new();
+
+        for x in 0..size as i32 {
+            vec.push(x);
+        }
+
         c.bench_with_input(
             BenchmarkId::new("Vec<i32>::get (random)", size),
             &size,
@@ -45,6 +79,19 @@ fn bench_get(c: &mut Criterion) {
                 )
             },
         );
+    }
+}
+
+fn bench_get_im_vec(c: &mut Criterion) {
+    let mut rng = thread_rng();
+
+    for size in [1_000, 10_000, 100_000] {
+        let mut im_vec = im::Vector::new();
+
+        for x in 0..size as i32 {
+            let i = rng.gen_range(0..=im_vec.len());
+            im_vec.insert(i, x);
+        }
 
         c.bench_with_input(
             BenchmarkId::new("im::Vector<i32>::get (random)", size),
@@ -66,14 +113,12 @@ fn bench_insert(c: &mut Criterion) {
     for size in [1_000, 10_000, 100_000, 1_000_000] {
         let mut bvec = BTreeVec::<i32>::new();
         let mut vec = Vec::new();
-        let mut im_vec = Vector::new();
 
         for x in 0..size as i32 {
             let i = rng.gen_range(0..=bvec.len());
 
             bvec.insert(i, x);
-            vec.insert(i, x);
-            im_vec.insert(i, x);
+            vec.push(x);
         }
 
         c.bench_with_input(
@@ -105,27 +150,12 @@ fn bench_insert(c: &mut Criterion) {
                 )
             },
         );
-
-        c.bench_with_input(
-            BenchmarkId::new("im::Vector<i32>::insert_remove (random)", size),
-            &size,
-            |b, &s| {
-                b.iter_batched(
-                    || rng.gen_range(0..=s),
-                    |i| {
-                        im_vec.insert(i, 0);
-                        im_vec.remove(i)
-                    },
-                    BatchSize::SmallInput,
-                )
-            },
-        );
     }
 }
 
 criterion_group!(
     name = benches;
     config = Criterion::default().sample_size(500);
-    targets = bench_get, bench_insert
+    targets = bench_get_bvec, bench_get_vec, bench_get_im_vec, bench_insert
 );
 criterion_main!(benches);
